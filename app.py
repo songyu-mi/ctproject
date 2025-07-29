edbc906d7992d2742147c57e6d85fcd7b778f08d
songyu-mi <songyumi07@gmail.com>
2025-07-29 13:06:39 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import streamlit.components.v1 as htmlviewer
-st.set_page_config(layout='wide', page_title='DNA sequencying!!!')
-# Title Msg#1
-st.title('This is Yum Webapp!!')
+st.set_page_config(layout='wide', page_title='Math problem!!!')
+# title msg1#
+st.title('This is YUM WEBAPP!!')
 
 with open('./index copy 3.html', 'r', encoding='utf-8') as f:
     html = f.read()
-    f.close()
 
 #html = '''
 #<html>
@@ -19,18 +18,34 @@ with open('./index copy 3.html', 'r', encoding='utf-8') as f:
 #    </body>
 #</html>
 #'''
+
 # Box#1(4), Box#2(1)
-col1, col2 = st.columns((4,1))
+col1, col2 = st.columns((4, 1))
+
 with col1:
     with st.expander('Content #1...'):
         url = 'https://www.youtube.com/watch?v=XyEOEBsa8I4'
         st.info('Content..')
         st.video(url)
+
+    with st.expander('image content...'):
+        imgfilepath = './images/gpt.jpg'
+        st.image(imgfilepath)
+
     with st.expander('Content #2...'):
-        #st.write(html, unsafe_allow_html=True)
-        htmlviewer.html(html, width=1000, height=800)
+        with open('index copy 3.html', 'r', encoding='utf-8') as f:
+            other_html = f.read()
+        htmlviewer.html(other_html, height=1000)
+
+    with st.expander('Content #3...'):
+        with open('index copy 4.html', 'r', encoding='utf-8') as f:
+            other_html = f.read()
+        htmlviewer.html(other_html, height=1000)
+
+
 with col2:
     with st.expander('Tips..'):
         st.info('Tips..')
+
 st.markdown('<hr>', unsafe_allow_html=True)
 st.write('<font color="BLUE">(c)copyright. all rights reserved by Yum', unsafe_allow_html=True)
